a8064c57e0d4924620eff543fcabc9f13b7f0aa4
SDINAHET <stephane.dinahet@gmail.com>
2024-08-05 20:51:32 +0200

    fix SD 1 et 2 	/* Check if the parent pointer is NULL */ 	if (parent == NULL) 	{ 		return (NULL); 	}

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,11 +12,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
+	/* Check if the parent pointer is NULL */
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
-
 
 	/*Allocate memory for the new node*/
 	new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
