58e3772b54f0079d9fb7769e0d65bc8183e4436e
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-08-06 17:30:54 +0200

    just trying some random ideas that are popping in my head in this version of the code i deleted the height function and included the 9 binary directly in the include part of the code i don't think it should work but hey if i can get at least 1 point

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,24 +1,6 @@
 #include "binary_trees.h"
 #include <stdlib.h>
-
-/**
- * binary_tree_height - Measures the height of a binary tree.
- * @tree: A pointer to the root node of the tree.
- *
- * Return: The height of the tree.
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-	{
-		return (0);
-	}
-
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
-
-	return (1 + (left_height > right_height ? left_height : right_height));
-}
+#include "9-binary_tree_height.c"
 
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree.
