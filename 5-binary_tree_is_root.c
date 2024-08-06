c08f3f79220231fc39957485fdabf3a423677c9b
SDINAHET <stephane.dinahet@gmail.com>
2024-08-06 09:30:20 +0200

    task5 projet28

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,5 +8,13 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-
+	if (node == NULL)
+	{
+		return (0);
+	}
+	if (node->parent == NULL) /*if (node->root == NULL) ne marche pas ici root*/
+	{
+		return (1);
+	}
+	return (0);
 }
