e9d9f10c365ca75953eeaf21f947d6ace4bbd489
SDINAHET <stephane.dinahet@gmail.com>
2024-08-06 11:06:42 +0200

    task9 projet28

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,5 +8,26 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 
+	/* Recursively compute the height of the left and right subtrees */
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	/* Return the maximum of the two heights plus one */
+	/*return (1 + (left_height > right_height ? left_height : right_height));*/
+
+	size_t max_height;
+
+	if (left_height > right_height)
+	{
+		max_height = left_height;
+	}
+	else
+	{
+		max_height = right_height;
+	}
+
+	return (1 + max_height);
 }
