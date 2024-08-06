ac77f3d082a885ad810589d6052872bb683ad1ce
SDINAHET <stephane.dinahet@gmail.com>
2024-08-06 16:28:17 +0200

    task12 projet28

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,5 +8,14 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 
+	/* Check if the current node is a leaf */
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	/* Recursively count leaves in the left and right subtrees */
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
+
