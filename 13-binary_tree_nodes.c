8ee4439d1d9f43ba0ed3497f2bd37d6c526d6ab5
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-08-06 16:43:45 +0200

    fixed betty for 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,7 +8,7 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t	count = 0;
+	size_t count = 0;
 
 	if (tree == NULL)
 		return (0);
@@ -21,7 +21,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	}
 	else
 	{
-		/* Count nodes in the left and right subtrees without counting the current node*/
+		/* Count nodes in the left and right subtrees without counting the node*/
 		count = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
 	}
 
