26c8f0a998ea98f23f701d43688078d4ba2cde7d
SDINAHET <stephane.dinahet@gmail.com>
2024-08-06 16:10:09 +0200

    task11 projet28

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,5 +8,9 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 
+	/* Recursively get the size of the left and right subtrees */
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
