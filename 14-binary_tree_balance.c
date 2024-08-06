3dfb6932c8f9ba9ec79749ee47a9bcafdb6a5fe9
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-08-06 16:58:30 +0200

    made 14 (not ready yet)

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,15 +1,21 @@
 #include "binary_trees.h"
 #include <stdlib.h>
+#include "9-binary_tree_height.c"
 /**
  * binary_tree_height - Measures the height of a binary tree.
  * @tree: A pointer to the root node of the tree.
- *
+
  * Return: The height of the tree.
  */
 int binary_tree_balance(const binary_tree_t *tree)
-
-========== 14-binary_tree_balance.c ==========
-14-binary_tree_balance.c:8: warning: no description found for function binary_tree_balance
 {
 
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
