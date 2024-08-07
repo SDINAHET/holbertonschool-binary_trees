6931e76176d6465a0bbcf0f360f63e9a26877375
MrSandouiche2 <zemihimehdi1@gmail.com>
2024-08-07 10:37:57 +0200

    fixed 14 (moved the declarations)

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,11 +9,13 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	size_t left_height, right_height;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	size_t left_height, right_height;
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
